d977fdc272cf7ac4301e682ae8d3ed8bb828ba25
yilin <yilin@yilins-M4-Pro.local>
2025-11-05 19:50:41 +0800

    Update random_vector.cpp

--- lab1/RandomVector/random_vector.cpp
+++ lab1/RandomVector/random_vector.cpp
@@ -1,24 +1,55 @@
 #include "random_vector.h"
-// TODO: add any include you might require
+#include <cfloat>
+#include <iostream>
+#include <random>
+// #include <algorithm>
+// #include <numeric>
 
-RandomVector::RandomVector(int size, double max_val) { 
-  // TODO: Write your code here
+RandomVector::RandomVector(int size, double max_val) {
+    std::random_device rd;
+    std::mt19937 eng(rd()); // Mersenne Twister 19937
+    std::uniform_real_distribution<double> distr(0.0, max_val);
+    for (int i = 0; i < size; i++) {
+        vect.push_back(distr(eng));
+    }
 }
 
-void RandomVector::print(){
-  // TODO: Write your code here
+void RandomVector::print() {
+    for (double it : vect) {
+        std::cout << it << std::endl;
+    }
 }
 
-double RandomVector::mean(){
-  // TODO: Write your code here
+double RandomVector::mean() {
+    double mean_to_return = 0.0;
+    for (double it : vect) {
+        mean_to_return += it;
+    }
+    mean_to_return /= vect.size();
+    return mean_to_return;
+    // return std::accumulate(vect.begin(), vect.end(), 0);
 }
 
 double RandomVector::max(){
-  // TODO: Write your code here
+    double max_to_return = 0.0;
+    for (double it : vect) {
+        if (max_to_return < it) {
+            max_to_return = it;
+        }
+    }
+    return max_to_return;
+    // return *std::max_element(vect.begin(), vect.end()); // just like *void in C
 }
 
 double RandomVector::min(){
-  //TODO:  Write your code here
+    double min_to_return = DBL_MAX;
+    for (double it : vect) {
+        if (it < min_to_return) {
+           min_to_return = it;
+        }
+    }
+    return min_to_return;
+    // return *std::min_element(vect.begin(), vect.end());
 }
 
 void RandomVector::printHistogram(int bins){
